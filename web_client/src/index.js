47bf0310680108958ae330ee6611dbb9be865de1
mrrbrilliant <mrrbrilliantwork@gmail.com>
2020-08-14 14:40:53 +0700

    update

--- web_client/src/index.js
+++ web_client/src/index.js
@@ -5,13 +5,31 @@ import "./assets/css/index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// Apollo
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { createHttpLink } from "apollo-link-http";
+const link = createHttpLink({
+  uri: "http://192.168.1.120:3300/",
+  credentials: "same-origin",
+  //   useGETForQueries: false,
+  //   fetchOptions: {
+  //     mode: "no-cors",
+  //   },
+});
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
