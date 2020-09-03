2efb9d5a182f708b2a362bba1d79430cf623e697
mrrbrilliant <mrrbrilliantwork@gmail.com>
2020-09-03 23:25:45 +0700

    removed node server since successfully moved to rust

--- node_server/models/role.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const ROLE = new Schema({
-	name: String,
-	description: String,
-});
-
-module.exports = mongoose.model("ROLE", ROLE);
