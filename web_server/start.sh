e6793bff85066c4cfd4114915cfeba37f2948512
mrrbrilliant <mrrbrilliantwork@gmail.com>
2020-09-03 23:29:05 +0700

    remove web server using actix + juniper + r2d2 since move to actix + async_graphql + mongodb 1

--- web_server/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-which systemfd > /dev/null
-[[ $? -eq 1 ]] && cargo install systemfd 
-which cargo-watch > /dev/null
-[[ $? -eq 1 ]] && cargo install cargo-watch
-systemfd --no-pid -s http::3300 -- cargo watch -x run
--- web_server/stop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kill -9 $(lsof -t -i:3300)
